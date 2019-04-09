31397001e3a139151776e48b22bba1d97c74441e
CetaceanNation <gamer.72.365@gmail.com>
2019-04-09 12:55:24 -0400

    updated with some modification for separate key and plaintext input, needs output reworking

--- KeyMaker.py
+++ KeyMaker.py
@@ -18,18 +18,29 @@ def make_stream_source(key):
         stream[j] = temp
     return stream
 
-
+def gen_stream(key, source):
+    i = 0
+    j = 0
+    while True:
+        i = (i + 1) % 256
+        j = (j + source[i]) % 256
+        temp = source[i]
+        source[i] = source[j]
+        source[j] = temp
+        k = source[(source[i] + source[j]) % 256]
+        print(chr(k))
 
 def main():
     if len(sys.argv) == 2:
         keyinput = sys.argv[1]
     else:
-        keyinput = input('enter a string for encrypting: ')
+        keyinput = input('enter a key to use: ')
+        plaintext = input('enter a string for encrypting: ')
     keyinput = key_to_bytes(keyinput)
     print('key:', keyinput)
     stream_source = make_stream_source(keyinput)
     print('stream source:', stream_source)
-    
+    gen_stream(keyinput, stream_source)
     
 if __name__ == '__main__':
     main()
